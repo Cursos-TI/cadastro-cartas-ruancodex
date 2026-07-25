3e620bd767385e833422ba723c8cc104dc1c43e9
Ruan Rodrigues Da Silva <rrs.ruann@gmail.com>
2026-07-25 23:26:29 +0000

    adicionei a opcao de super poder

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -9,8 +9,9 @@
     float area_em_km1;
     float pib1;
     int numero_de_pontos_turisticos1;
-    float densidade_populacional1, pib_per_capita1;
-
+    float densidade_populacional1, pib_per_capita1, inverso_da_densidade_populacional1;
+    float super_poder1;
+    
     //carta 2
     char estado2[50];
     char codigo_da_carta2[50];
@@ -18,7 +19,8 @@
     float area_em_km2;
     float pib2;
     int numero_de_pontos_turisticos2;
-    float densidade_populacional2, pib_per_capita2;
+    float densidade_populacional2, pib_per_capita2, inverso_da_densidade_populacional2;
+    float super_poder2;
 
    // Área para entrada de dados
     printf ("informacoes da carta 1 \n");
@@ -57,27 +59,37 @@
     densidade_populacional2 = populacao2 / area_em_km2;
     pib_per_capita2 = pib2 / populacao2; 
 
-   // Área para exibição dos dados da cidade
+    inverso_da_densidade_populacional1 = area_em_km1 / populacao1;
+    inverso_da_densidade_populacional2 = area_em_km2 / populacao2;
+
+    super_poder1 = populacao1 + area_em_km1 + pib1 + numero_de_pontos_turisticos1 + pib_per_capita1 + inverso_da_densidade_populacional1;
+    super_poder2 = populacao2 + area_em_km2 + pib2 + numero_de_pontos_turisticos2 + pib_per_capita2 + inverso_da_densidade_populacional2;
+
+    // Área para exibição dos dados da cidade
 
     printf("\n====================carta 1===================\n");
-    printf("estado: %s \n",estado1);
-    printf("codigo da carta: %s \n",codigo_da_carta1);
-    printf("populacao: %d \n",populacao1);
-    printf("area em km: %.3f \n",area_em_km1);
-    printf("pib: %.3f bilhoes de reais \n",pib1);
-    printf("numero de pontos turisticos: %d \n",numero_de_pontos_turisticos1);
-    printf("densidade populacional: %.3f hab/km2 \n",densidade_populacional1);
-    printf("pib per capita: %.3f \n", pib_per_capita1);
+    printf("estado: %s \n", estado1);
+    printf("codigo da carta: %s \n", codigo_da_carta1);
+    printf("populacao: %d \n", populacao1);
+    printf("area em km: %.3f km\n", area_em_km1);
+    printf("pib: %.3f bilhoes de reais \n", pib1);
+    printf("numero de pontos turisticos: %d \n", numero_de_pontos_turisticos1);
+    printf("densidade populacional: %.2f hab/km2 \n", densidade_populacional1);
+    printf("pib percapita: %.2f \n", pib_per_capita1);
+    printf("super poder: %.2f\n", super_poder1);
 
     printf("\n====================carta 2====================\n");
     printf("estado: %s \n",estado2);
-    printf("codigo da carta: %s \n",codigo_da_carta2);
-    printf("populacao: %d \n",populacao2);
-    printf("area em km: %.3f \n",area_em_km2);
-    printf("pib: %.3f bilhoes de reais \n",pib2);
-    printf("numero de pontos turisticos: %d \n",numero_de_pontos_turisticos2);
-    printf("densidade populacional: %.3f hab/km2 \n",densidade_populacional2);
-    printf("pib percapita: %.3f \n", pib_per_capita2);
+    printf("codigo da carta: %s \n", codigo_da_carta2);
+    printf("populacao: %d \n", populacao2);
+    printf("area em km: %.3f km\n", area_em_km2);
+    printf("pib: %.3f bilhoes de reais \n", pib2);
+    printf("numero de pontos turisticos: %d \n", numero_de_pontos_turisticos2);
+    printf("densidade populacional: %.2f hab/km2 \n", densidade_populacional2);
+    printf("pib percapita: %.2f \n", pib_per_capita2);
+    printf("super poder: %.2f\n", super_poder2);
+
+
 
 return 0;
 } 
